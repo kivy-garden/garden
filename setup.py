b04ed2db7c272f72a658fb8fd499ce751dbdfb8b
Ian Foote <python@ian.feete.org>
2013-09-26 20:15:44 +0100

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='Kivy Garden',
+        version='0.1dev',
+        license='MIT',
+        scripts=['garden', 'garden.bat'],
+        install_requires=['requests'],
+        )
