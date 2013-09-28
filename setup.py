08739047ee2710850eab7b6c77a2bdf8b2a6201a
Ian Foote <python@ian.feete.org>
2013-09-28 14:56:56 +0100

    Release version 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='Kivy Garden',
-        version='0.1dev',
+        version='0.1',
         license='MIT',
         scripts=['garden', 'garden.bat'],
         install_requires=['requests'],
